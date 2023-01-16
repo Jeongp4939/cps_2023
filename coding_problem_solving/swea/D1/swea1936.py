01d4a6d51d70f11838aaaaf865f7f8dfa501ad0a
박정훈 <jeongp12@gmail.com>
2023-01-16 17:38:39 +0900

    230116 coding problem commit move directory_2

--- coding_problem_solving/swea/D1/swea1936.py
+++ coding_problem_solving/swea/D1/swea1936.py
@@ -1,7 +1,7 @@
 # 1:1 가위바위보, 가위는 1, 바위는 2, 보는 3
 
 a, b = map(int, input().split())
-if (a-b)%3 ==1:
+if a - b == 1 or (a == 1 and b == 3):
     print('A')
 else:
     print('B')
---
+++ coding_problem_solving/swea/D2/swea1926.py
@@ -0,0 +1,8 @@
+# 369 게임
+N = int(input())
+
+for i in range(1,N+1):
+    if '3' in str(i) or '6' in str(i) or '9' in str(i):
+        print('-'*(str(i).count('3')+str(i).count('6')+str(i).count('9')),end=' ')
+    else:
+        print(i, end=' ')
