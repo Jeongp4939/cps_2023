a8d76c2975d6f317a7f5224744646b6e8362cc76
박정훈 <jeongp12@gmail.com>
2023-02-01 15:06:39 +0900

    snail.py

--- coding_problem_solving/class_problem/달팽이_숫자.py
+++ coding_problem_solving/class_problem/달팽이_숫자.py
@@ -6,18 +6,18 @@ for tc in range(1, T + 1):
     n = int(input())
     snail = [[0] * n for _ in range(n)]
     y, x = 0, 0
-    dir = 0
+    direction = 0
     for i in range(1, n * n + 1):
         snail[y][x] = i
-        y += dy[dir]
-        x += dx[dir]
+        y += dy[direction]
+        x += dx[direction]
 
         if x < 0 or y < 0 or x >= n or y >= n or snail[y][x] != 0:
-            y -= dy[dir]
-            x -= dx[dir]
-            dir = (dir + 1) % 4
-            y += dy[dir]
-            x += dx[dir]
+            y -= dy[direction]
+            x -= dx[direction]
+            direction = (direction + 1) % 4
+            y += dy[direction]
+            x += dx[direction]
     print(f'#{tc}')
 
     for line in snail:
