f465eeeb0ef51b3baf2ca78de959f5c9c194bf83
jeongp4939 <jeongp12@gmail.com>
2023-02-20 22:15:37 +0900

    230220 home commit

--- coding_problem_solving/class_problem/노드의거리.py
+++ coding_problem_solving/class_problem/노드의거리.py
@@ -1,22 +1,16 @@
 from collections import deque
 
 def BFS(st,ed):
-
-    q = deque()
-    q.append(st)
+    q = deque([st])
     visited[st]=1
-    cnt = 0
     while q:
-        cnt += 1
         node = q.popleft()
-        if ed in lst[node]:
-            return visited[i]
-        for i in lst[node]:
-            if visited[i]==0:
-                visited[i]=visited[i]+1
-                q.append(i)
-            if ed in lst[i]:
-                return visited[i]
+        for n in lst[node]:
+            if visited[n]==0:
+                if n == ed:
+                    return visited[node]
+                visited[n]=visited[node]+1
+                q.append(n)
     return 0
 
 for tc in range(1,int(input())+1):
