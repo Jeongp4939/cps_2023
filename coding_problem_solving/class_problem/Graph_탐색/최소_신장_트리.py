6eb8bcda495af8ee5d52327d41261245c560014f
Jeong4939 <jeongp12@gmail.com>
2023-04-04 17:33:38 +0900

    230404 commit

--- coding_problem_solving/class_problem/Graph_탐색/최소_신장_트리.py
+++ coding_problem_solving/class_problem/Graph_탐색/최소_신장_트리.py
@@ -1,26 +1,33 @@
+def find(x):
+    if x != graph[x]:
+        return find(graph[x])
+    return graph[x]
 
-def dfs(depth, st, w=0):
-    global Min
-    if depth == v:
-        Min = min(Min,w)
-        return
-    for i in graph[st]:
-        if not visited[i[0]]:
-            visited[i[0]] = 1
-            dfs(depth+1, i[0], w+i[1])
-            visited[i[0]] = 0
 
-for tc in range(1, 1+int(input())):
-    v,e = map(int,input().split())
-    graph = [[] for _ in range(v+1)]
-    for _ in range(e):
-        n1,n2,w = map(int,input().split())
-        graph[n1].append((n2,w))
-        graph[n2].append((n1,w))
+def union(a, b):
+    a, b = find(a), find(b)
+    if a > b:
+        graph[a] = b
+    else:
+        graph[b] = a
 
-    Min =int(21e8)
-    for i in range(v+1):
-        visited = [0]*(v+1)
-        visited[i]=1
-        dfs(0,i)
-    print(f'#{tc} {Min}')
+
+for testcase in range(1, int(input()) + 1):
+    v, e = map(int, input().split())
+    lst = [list(map(int, input().split())) for _ in range(e)]
+    # 가중치를 기준으로 정렬
+    sorted_lst = sorted(lst,key=lambda x:x[2])
+    graph = [0] * (v + 1)
+    result = 0
+    cnt = 0
+    for i in range(v + 1):
+        graph[i] = i
+    for i in sorted_lst:
+        start, end, cost = i
+        if find(start) != find(end):
+            cnt += 1
+            result += cost
+            union(start, end)
+        if cnt == v:
+            print(f'#{testcase} {result}')
+            break
